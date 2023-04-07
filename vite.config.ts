0d97c9b0139bf607664de8ae1a547a3d56c5f7b2
Pierre Leroyer <46136882+InSomniaMoon@users.noreply.github.com>
2023-04-07 16:31:10 +0200

    add vite config for scss & ts (#4)

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import path from "path";
+import process from "process";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+    cors: true,
+
+  },
+  root: "src/",
+  build: {
+    outDir: "./dist"
+  },
+  resolve: {
+    alias: { "/src": path.resolve(process.cwd(), "src") }
+  },
+});
